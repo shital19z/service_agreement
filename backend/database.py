b6a7a13a506b2a71512ea342e67e7e03554dc61a
shital19z <shitalzile.sfdc@gmail.com>
2026-02-23 14:00:45 +0530

    Fix database loading logic and add gitignore

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost/service_agreement_db"
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
