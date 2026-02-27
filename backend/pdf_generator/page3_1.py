0e182ec41efa851cae076904bb09e1c682e1810e
shital19z <shitalzile.sfdc@gmail.com>
2026-02-27 10:19:34 +0530

    changes in logo and fixed the branch

--- backend/pdf_generator/page3_1.py
+++ backend/pdf_generator/page3_1.py
@@ -14,80 +14,90 @@ def generate_page3_1(data):
     
     html = '<div>'
     
-    # Logo header - reduced margin
+    # Logo header - MOVED TO RIGHT SIDE
     html += f'''
-    <table width="100%" style="font-size:10px; margin-bottom:20px;">
+    <table width="100%" style="font-size:11.5px; margin-bottom:50px;" cellpadding="0" cellspacing="0">
         <tr>
-            <td style="padding-left:10px;">
-                <img style="width:2in;height:0.9in;" src="{logo_path}" />
+            <td width="70%">&nbsp;</td>  <!-- Empty space on left -->
+            <td width="30%" align="right" style="padding-right:18px;">  <!-- Logo on right -->
+                <img style="width:2.0833in;height:0.9166in;" src="{logo_path}" />
             </td>
         </tr>
     </table>
     '''
     
-    # Title - reduced font sizes and margins
+    # Title
     html += '''
-    <p style="text-align: center; font-weight:700; font-size: 16pt; font-family: Calibri; font-style: normal; text-decoration: underline; margin:5px 0;"><b>Consumer Notice of Direct Care Worker Status</b></p>
-    <p style="text-align: center; font-weight:400; font-size: 10pt; font-family: Calibri; font-style: normal; margin:2px 0;">This form is to be completed by every consumer utilizing the <br> services of a Home Care Agency or Home Care Registry</p>
+    <p style="text-align: center; font-weight:700; font-size: 18pt; font-family: Calibri; font-style: normal; text-decoration: underline; margin:0 0 10px 0;"><b>Consumer Notice of Direct Care Worker Status</b></p>
+    <p style="text-align: center; font-weight:400; font-size: 12pt; font-family: Calibri; font-style: normal; margin:0 0 20px 0;">This form is to be completed by every consumer utilizing the <br> services of a Home Care Agency or Home Care Registry</p>
     '''
     
-    # First section - Name - reduced spacing
+    # First section - Name
     html += f'''
-    <table width="100%" style="font-size:10px; margin-top:15px;">
+    <table width="100%" style="font-size:12pt; margin-top:20px;" cellpadding="0" cellspacing="0">
         <tr>
-            <td style="padding-left:10px;" width="5%">
-                <p style="font-weight:400; font-size: 11pt; font-family: Calibri; margin:0;">I,</p>
+            <td style="padding-left:18px;" width="5%" valign="top">
+                <p style="font-weight:400; font-size: 12pt; font-family: Calibri; font-style: normal; margin:0;">I,</p>
             </td>
-            <td width="55%">
-                <p style="text-align: center; font-weight:400; font-size: 11pt; font-family: Calibri; margin:0;"><b>{clt_first} {clt_last}</b></p>
-                <div style="border-top:1.5px solid black; width:90%; margin:2px auto;"></div>
-                <p style="text-align: center; font-weight:400; font-size: 10pt; font-family: Calibri; margin:1px 0;">(Print Name)</p>
+            <td width="55%" valign="top">
+                <p style="text-align: center; font-weight:400; font-size: 12pt; font-family: Calibri; font-style: normal; margin:0;"><b>{clt_first} {clt_last}</b></p>
+                <div style="border-top:2px solid black; width:95%; margin:5px auto 2px auto;"></div>
+                <p style="text-align: center; font-weight:400; font-size: 10pt; font-family: Calibri; font-style: normal; margin:0;">(Print Name)</p>
             </td>
-            <td width="40%">
-                <p style="text-align: left; font-weight:400; font-size: 11pt; font-family: Calibri; margin:0; padding-top:5px;"> understand that:</p>
+            <td width="40%" valign="top">
+                <p style="text-align: left; font-weight:400; font-size: 12pt; font-family: Calibri; font-style: normal; margin:0;"> understand that:</p>
             </td>
         </tr>
     </table>
     '''
     
-    # Second section - Insurance information - reduced spacing
+    # Second section - Initials and Insurance information
     html += '''
-    <table width="100%" style="font-size:10px; margin-top:15px;">
+    <table width="100%" style="font-size:12pt; margin-top:25px;" cellpadding="0" cellspacing="0">
         <tr>
-            <td style="padding-left:10px;" width="15%">
-                <div style="border-top:1.5px solid black; width:80%; margin:0 auto;"></div>
-                <p style="text-align: center; font-weight:400; font-size: 10pt; font-family: Calibri; margin:1px 0;">Initials</p>
+            <td style="padding-left:18px;" width="20%" valign="top">
+                <div style="border-top:2px solid black; width:80%; margin:0 auto 2px auto;"></div>
+                <p style="text-align: center; font-weight:400; font-size: 11pt; font-family: Calibri; font-style: normal; margin:0;">Initials</p>
             </td>
-            <td style="padding-left:5px;" width="85%">
-                <p style="text-align: left; font-weight:400; font-size: 10pt; font-family: Calibri; margin:0; line-height:1.2;">I have been informed that Options For Senior America maintains general, professional liability, and workers compensation insurance covering the direct care worker who is employed by Options as an independent contractor.</p>                
+            <td width="80%" valign="top">
+                <p style="text-align: left; font-weight:400; font-size: 11pt; font-family: Calibri; font-style: normal; margin:0; line-height:1.3;">I have been informed that Options For Senior America maintains general, professional liability, and workers compensation insurance covering the direct care worker who is employed by Options as an independent contractor.</p>                
             </td>
         </tr>
     </table>
     '''
     
-    # Signature section - reduced spacing
+    # Signature section - SIMPLIFIED to avoid table rendering issues
+    current_date = datetime.now().strftime("%m/%d/%Y")
+    
+    # First signature row (consumer)
     html += f'''
-    <table width="100%" style="font-size:10px; margin-top:25px;">
+    <table width="100%" style="font-size:12pt; margin-top:40px;" cellpadding="0" cellspacing="0">
         <tr>
-            <td align="center" width="70%" style="padding-right:5px;">
-                <div style="border-top:1.5px solid black; width:80%; margin:0 auto;"></div>
-                <p style="font-weight:400; font-size: 10pt; font-family: Calibri; margin:2px 0;">Signature of Consumer or Consumer\'s Representative</p>
+            <td width="70%" align="center" style="padding-right:10px;">
+                <div style="border-top:2px solid black; width:90%; margin:0 auto 2px auto;"></div>
+                <p style="font-weight:400; font-size: 11pt; font-family: Calibri; font-style: normal; margin:0;">Signature of Consumer or Consumer\'s Representative</p>
             </td>
-            <td align="center" width="30%" style="padding-left:5px;">
-                <div style="border-top:1.5px solid black; width:80%; margin:0 auto;"></div>
-                <p style="font-weight:400; font-size: 10pt; font-family: Calibri; margin:2px 0;">Date</p>
+            <td width="30%" align="center" style="padding-left:10px;">
+                <div style="border-top:2px solid black; width:80%; margin:0 auto 2px auto;"></div>
+                <p style="font-weight:400; font-size: 11pt; font-family: Calibri; font-style: normal; margin:0;">Date</p>
             </td>
         </tr>
+    </table>
+    '''
+    
+    # Second signature row (representative) - completely separate table
+    html += f'''
+    <table width="100%" style="font-size:12pt; margin-top:30px;" cellpadding="0" cellspacing="0">
         <tr>
-            <td align="center" width="70%" style="padding-top:20px; padding-right:5px;">
-                <p style="font-weight:400; font-size: 11pt; font-family: Calibri; margin:0;"><b>{handled_by}</b></p>
-                <div style="border-top:1.5px solid black; width:80%; margin:5px auto 0;"></div>
-                <p style="font-weight:400; font-size: 10pt; font-family: Calibri; margin:2px 0;">Signature of Representative of Options For Senior America</p>
+            <td width="70%" align="center" style="padding-right:10px;">
+                <p style="font-weight:400; font-size: 12pt; font-family: Calibri; font-style: normal; margin:0 0 5px 0;"><b>{handled_by}</b></p>
+                <div style="border-top:2px solid black; width:90%; margin:0 auto 2px auto;"></div>
+                <p style="font-weight:400; font-size: 11pt; font-family: Calibri; font-style: normal; margin:0;">Signature of Representative of Options For Senior America</p>
             </td>
-            <td align="center" width="30%" style="padding-top:20px; padding-left:5px;">
-                <p style="font-weight:400; font-size: 11pt; font-family: Calibri; margin:0;"><b>{datetime.now().strftime("%m/%d/%Y")}</b></p>
-                <div style="border-top:1.5px solid black; width:80%; margin:5px auto 0;"></div>
-                <p style="font-weight:400; font-size: 10pt; font-family: Calibri; margin:2px 0;">Date</p>
+            <td width="30%" align="center" style="padding-left:10px;">
+                <p style="font-weight:400; font-size: 12pt; font-family: Calibri; font-style: normal; margin:0 0 5px 0;"><b>{current_date}</b></p>
+                <div style="border-top:2px solid black; width:80%; margin:0 auto 2px auto;"></div>
+                <p style="font-weight:400; font-size: 11pt; font-family: Calibri; font-style: normal; margin:0;">Date</p>
             </td>
         </tr>
     </table>
